710ecb72f49a1e04236753285a97a44df324a685
Jeloves <anjelovaleraecube@icloud.com>
2024-02-11 18:34:28 -0500

    buildProps function and template prop data

--- map/grid.js
+++ map/grid.js
@@ -1,9 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Tile = void 0;
+var map_data_1 = require("./map_data");
 var Tile = /** @class */ (function () {
     function Tile(x, y, north, east, south, west) {
         if (north === void 0) { north = 0; }
         if (east === void 0) { east = 0; }
         if (south === void 0) { south = 0; }
         if (west === void 0) { west = 0; }
+        // Objects (Unit || Prop)
+        this.object = null;
         this.x = x;
         this.y = y;
         this.north = north;
@@ -13,16 +19,22 @@ var Tile = /** @class */ (function () {
     }
     return Tile;
 }());
+exports.Tile = Tile;
 function generateMap(xMax, yMax) {
-    // Creating array to represent map-grid
-    var grid = [];
-    for (var y = 0; y < yMax; y++) {
+    // Creating a 2D array to represent the map
+    var map = [];
+    for (var y = 1; y <= yMax; y++) {
         var row = [];
-        for (var x = 0; x < xMax; x++) {
+        for (var x = 1; x <= xMax; x++) {
             var tile = new Tile(x, y);
             row.push(tile);
         }
-        grid.push(row);
+        map.push(row);
     }
-    return grid;
+    console.log(map);
+    return map;
 }
+var map = generateMap(20, 20);
+(0, map_data_1.buildProps)(map, map_data_1.propCoordinates);
+console.log(map);
+console.log("done");
